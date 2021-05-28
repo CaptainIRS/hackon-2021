6b7b9dd31d567995993f5597ecb4f9bf7fc82520
Anirudh V S <anirudh.vs@outlook.com>
2021-05-28 14:27:22 +0530

    (lint): fix

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -8,14 +8,14 @@ import Home from '../routes/home';
 import Profile from '../routes/profile';
 
 const App = () => (
-	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
-	</div>
+  <div id="app">
+    <Header />
+    <Router>
+      <Home path="/" />
+      <Profile path="/profile/" user="me" />
+      <Profile path="/profile/:user" />
+    </Router>
+  </div>
 )
 
 export default App;
