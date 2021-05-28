8759885e06a765336843c25fa346444792a90676
Anirudh V S <anirudh.vs@outlook.com>
2021-05-29 00:22:49 +0530

    (ipfs): added IPFS util

---
+++ server/utils/ipfsHandler.js
@@ -0,0 +1,9 @@
+const ipfsClient = require("ipfs-http-client");
+const ipfs = ipfsClient.create();
+
+const addFileToIPFS = async (path) => {
+  const file = await ipfs.add(ipfsClient.globSource(path));
+  console.log(`Added File ${path} to ${file.cid.toString()}`);
+  return file.cid.toString();
+};
+module.exports = { addFileToIPFS };
